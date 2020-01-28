c61826e286fedb8c9a689f75579ff854305f8ad4
travis skyles <travis.skyles@gmail.com>
2020-01-27 21:18:47 -0800

    refactored to async

--- src/file.js
+++ src/file.js
@@ -5,9 +5,34 @@ const fs = require('fs');
 const event = require('./event');
 
 const read = util.promisify(fs.readFile);
-const write = util.promisify(fs.writeFile);
+// const write = util.promisify(fs.writeFile);
 
+async function readFile(file){
+  try{
+    let data = read(file);
+    let obj = await data.toString();
+    event.emit('readFile', obj);
+    return obj;
+  }
+  catch(error){
+    event.emit('Error', error);
+  }
+}
 
+async function writeFile(file, string){
+  try{
+    const upString = string.toUpperCase();
+    fs.writeFile(file, upString.toString(), (error => {
+      if(error){event.emit('Error', error);}
+      event.emit('writeFile', 'successfully wrote file');
+    }));
+  }
+  catch(error){
+    event.emit('Error', error);
+  }
+}
+
+module.exports = {readFile, writeFile};
 
 
 
