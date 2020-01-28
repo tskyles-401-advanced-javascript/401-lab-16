69494bd3023485de809bc870e8f1db6b0b72f341
travis skyles <travis.skyles@gmail.com>
2020-01-27 20:23:54 -0800

    added event.on to logger file

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,8 @@
 
 const event = require('./event');
 
-// still need event.on
+event.on('readFile', payload => log('readFile', payload));
+event.on('writeFile', payload => log('writeFile', payload));
 
 function log(event, payload){
   let time = new Date();
